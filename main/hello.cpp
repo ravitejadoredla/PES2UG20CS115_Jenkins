abb7e8545f9b63598f820d6a1ee8d0fa4f1c5d43
ravitejadoredla <92437289+ravitejadoredla@users.noreply.github.com>
2023-02-28 19:54:07 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout<<"My SRN: PES2UG20CS115\n";
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Assignment 2a, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
